a61a9e524b065b32fc766b572a9de9a9fe6db2eb
Kristina <116723770+NureKucheriavenkoKristina@users.noreply.github.com>
2025-06-10 17:46:23 +0300

    Delete server/src directory

--- server/src/routes/zoom.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { createZoomMeeting } = require('../controllers/zoom.controller');
-const { authenticateUser } = require('../middlewares/auth');
-
-const router = express.Router();
-
-router.post('/create-meeting', authenticateUser, createZoomMeeting);
-
-module.exports = router;
